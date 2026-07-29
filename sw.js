f9441b84794e5f142e53ccfa35cb67a234869ff6
Anirudh <141948385+shoppluse@users.noreply.github.com>
2026-07-29 23:42:53 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'hknf-flora-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './plantdetails.html',
+  './plants.json',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install Event
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
